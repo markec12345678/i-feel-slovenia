3ad0a38e55b4aaca7472751dff6c3aff18fdebb1
markec12345678 <robertpezdirc12@gmail.com>
2026-04-26 18:21:12 +0200

    feat: add 5 new gallery images

--- src/data/mediaGallery.ts
+++ src/data/mediaGallery.ts
@@ -40,5 +40,55 @@ export const MEDIA_GALLERY: MediaItem[] = [
     thumbnail: '/_b5ac7716-0ea1-42cf-8e1e-437a20b9faf7.jpeg',
     alt: 'The Drinkers nova zasedba 2026',
     year: 2026
+  },
+  {
+    id: 'img-gallery-1',
+    type: 'image',
+    title: 'Live nastop',
+    description: 'Energija na odru',
+    src: '/_09269780-39b2-4713-abbf-da2ab587cab9.jpeg',
+    thumbnail: '/_09269780-39b2-4713-abbf-da2ab587cab9.jpeg',
+    alt: 'The Drinkers live nastop',
+    year: 2024
+  },
+  {
+    id: 'img-gallery-2',
+    type: 'image',
+    title: 'Backstage',
+    description: 'Priprave na koncert',
+    src: '/_60286c7f-d781-43a1-8987-7734b511e016.jpeg',
+    thumbnail: '/_60286c7f-d781-43a1-8987-7734b511e016.jpeg',
+    alt: 'The Drinkers backstage',
+    year: 2024
+  },
+  {
+    id: 'img-gallery-3',
+    type: 'image',
+    title: 'Pivska noč',
+    description: 'Drink\'n\'Roll atmosfera',
+    src: '/_a93d6388-1619-42ac-9856-0169b49e7ee3.jpeg',
+    thumbnail: '/_a93d6388-1619-42ac-9856-0169b49e7ee3.jpeg',
+    alt: 'The Drinkers pivska noč',
+    year: 2024
+  },
+  {
+    id: 'img-gallery-4',
+    type: 'image',
+    title: 'Zasedba 2024',
+    description: 'Nova postava benda',
+    src: '/_b6eb8ada-663d-4b24-8d67-a0528c06f555.jpeg',
+    thumbnail: '/_b6eb8ada-663d-4b24-8d67-a0528c06f555.jpeg',
+    alt: 'The Drinkers zasedba 2024',
+    year: 2024
+  },
+  {
+    id: 'img-gallery-5',
+    type: 'image',
+    title: 'Koncertna luč',
+    description: 'Pod žarometi',
+    src: '/_d8f28266-ac2c-4aca-8965-3846724c3298.jpeg',
+    thumbnail: '/_d8f28266-ac2c-4aca-8965-3846724c3298.jpeg',
+    alt: 'The Drinkers pod žarometi',
+    year: 2024
   }
 ];
